0b18e0c9a1e31ea0d83fae892d0299e30ac0607a
Diego Luiz <diego.luiz@veinteractive.com>
2016-05-25 21:51:56 +0100

    Add test project

---
+++ AdvWorks/AdvWorks.Api.Tests/SalesControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvWorks.Api.Controllers;
+using NUnit.Framework;
+
+namespace AdvWorks.Api.Tests
+{
+    [TestFixture]
+    public class SalesControllerTest
+    {
+        [Test]
+        public void SalesControllerReceivesSaleAndReturns204()
+        {
+            var controller = new SalesController();
+            Sale sale = new Sale();
+            var ret = controller.Post(sale);
+
+            
+        }
+    }
+}
